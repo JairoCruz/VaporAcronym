ad84d827cca5deb93a7d7fd966081d1340816ac2
Jairo Cruz <azaveliz@gmail.com>
2021-01-13 14:53:37 -0600

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,9 +6,19 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    //app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     app.databases.use(.postgres(hostname: "localhost", username: "alb", password: "jairo", database: "alb"), as: .psql)
+    //app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    
+
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+    postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+    app.databases.use(.postgres(
+        configuration: postgresConfig
+    ), as: .psql)
+    } else {
+    
+    }
+
 
     //app.migrations.add(CreateTodo())
     app.migrations.add(CreateAcronym())
